855bbd58cb5e9655ec24ba8d21f9f9214a243601
Marina Katruk <m.katruk@outlook.com>
2022-02-18 16:43:22 +0300

    add consts & variables

---
+++ app.js
@@ -0,0 +1,48 @@
+const keyboard = document.querySelector('.key-container')
+const tileDisplay = document.querySelector('.tile-container')
+
+const wordle = 'SUPER'
+
+let currentRow = 0
+let currentTile = 0
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '≪'
+]
+
+const guessRows = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+]
+
