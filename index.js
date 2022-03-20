9be7e9088c613b779639985ab616fda20a9b99a8
Marina Katruk <m.katruk@outlook.com>
2022-03-20 13:36:35 +0200

    add backend logics

---
+++ index.js
@@ -0,0 +1,29 @@
+const PORT = 8000
+const axios = require("axios").default
+const express = require("express")
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+
+app.get('/word', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
+        params: {count: '5', wordLength: '5'},
+        headers: {
+          'x-rapidapi-host': 'random-words5.p.rapidapi.com',
+          'x-rapidapi-key': process.env.RAPID_ENV_KEY
+        }
+    }
+      
+    axios.request(options).then((response) => {
+        console.log(response.data)
+        res.json(response.data[0])
+    }).catch((error) => {
+        console.error(error)
+    })
+})
+
+app.listen(PORT, () => console.log('Server is running on port ' + PORT))
